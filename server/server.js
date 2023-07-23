62648caafce29154fe01d65e58a368ed2ac7ad14
Richard musanje <musanjerichard70@gmail.com>
2023-07-23 15:40:02 +0300

    first commit

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(express.json())
 
 app.get('/', async(req, res) =>{
     res.status(200).send({
-        message: "This is a test!!",
+        message: "This is the OpenAI server!!",
     })
 })
 
